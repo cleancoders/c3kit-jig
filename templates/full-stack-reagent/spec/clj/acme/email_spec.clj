3d0feb0698d30fcedb7bb15b98614e5f659dbe72
Alex Root-Roatch <arootroatch@gmail.com>
2026-05-12 23:51:08 -0500

    chore(t1): strip proprietary refs (AWS SDKs, my.datomic.com, bin/setup, :ses)
    - Remove com.amazonaws/aws-java-sdk-* deps from templates/full-stack-reagent/deps.edn
    - Remove my.datomic.com mvn-repos entry (Datomic Pro free tier is on Maven Central)
    - Delete templates/full-stack-reagent/bin/setup (CLI owns rename)
    - Delete :ses defmethod + AWS imports from acme.email (:to-log only ships)
    - Add a comment pointing users to AWS-SES re-add path
    
    Specs still pass against the template tree at HEAD.

--- templates/full-stack-reagent/spec/clj/acme/email_spec.clj
+++ templates/full-stack-reagent/spec/clj/acme/email_spec.clj
@@ -1,34 +1,13 @@
 (ns acme.email-spec
-  (:import (com.amazonaws.services.simpleemail.model Destination Message))
-  (:require [acme.email :as sut] [speclj.core :refer :all]))
+  (:require [acme.email :as sut]
+            [c3kit.apron.log]
+            [speclj.core :refer :all]))
 
 (describe "Email"
 
-  (context "ses-destination"
+  (context "->email-list"
 
-    (it "just a to address"
-      (let [^Destination dest (sut/ses-destination "micah@cleancoders.com" nil nil)]
-        (should= ["micah@cleancoders.com"] (.getToAddresses dest))
-        (should= [] (.getCcAddresses dest))
-        (should= [] (.getBccAddresses dest))))
-
-    (it "all addresses single"
-      (let [^Destination dest (sut/ses-destination "micah@cleancoders.com"
-                                                   "micahmartin@gmail.com"
-                                                   "micah@airworthy.co")]
-        (should= ["micah@cleancoders.com"] (.getToAddresses dest))
-        (should= ["micahmartin@gmail.com"] (.getCcAddresses dest))
-        (should= ["micah@airworthy.co"] (.getBccAddresses dest))))
-
-    (it "all addresses multiple"
-      (let [^Destination dest (sut/ses-destination ["micah@cleancoders.com" "micah2@cleancoders.com"]
-                                                   ["micahmartin@gmail.com" "micahmartin2@gmail.com"]
-                                                   ["micah@airworthy.co" "micah2@airworthy.co"])]
-        (should= ["micah@cleancoders.com" "micah2@cleancoders.com"] (.getToAddresses dest))
-        (should= ["micahmartin@gmail.com" "micahmartin2@gmail.com"] (.getCcAddresses dest))
-        (should= ["micah@airworthy.co" "micah2@airworthy.co"] (.getBccAddresses dest))))
-
-    (it "->email-list"
+    (it "converts to a list"
       (should= ["abc"] (sut/->email-list "abc"))
       (should= ["abc"] (sut/->email-list ["abc"]))
       (should= [] (sut/->email-list nil))
@@ -38,25 +17,14 @@
 
     )
 
-  (context "ses-message"
+  (context "client-send-email"
 
-    (it "subject and just text"
-      (let [^Message message (sut/ses-message "Hello" "Hi There!" nil)]
-        (should= "Hello" (.getData (.getSubject message)))
-        (should= "Hi There!" (.getData (.getText (.getBody message))))
-        (should= "<p>Hi There!</p>\n" (.getData (.getHtml (.getBody message))))))
-
-    (it "subject and just HTML"
-      (let [^Message message (sut/ses-message "Hello" nil "<h1>Wave</h1>")]
-        (should= "Hello" (.getData (.getSubject message)))
-        (should= "Please see the HTML content of this email." (.getData (.getText (.getBody message))))
-        (should= "<h1>Wave</h1>" (.getData (.getHtml (.getBody message))))))
-
-    (it "text and HTML"
-      (let [^Message message (sut/ses-message "Hello" "Hi There!" "<h1>Wave</h1>")]
-        (should= "Hello" (.getData (.getSubject message)))
-        (should= "Hi There!" (.getData (.getText (.getBody message))))
-        (should= "<h1>Wave</h1>" (.getData (.getHtml (.getBody message))))))
+    (it "to-log client logs subject + body"
+      (c3kit.apron.log/capture-logs
+        (sut/client-send-email {:client :to-log}
+          {:from "a@b" :to "c@d" :subject "Hi" :text "Body" :html "<p>Body</p>"}))
+      (should-contain "Hi" (c3kit.apron.log/captured-logs-str))
+      (should-contain "Body" (c3kit.apron.log/captured-logs-str)))
 
     )
 
--- templates/full-stack-reagent/src/clj/acme/email.clj
+++ templates/full-stack-reagent/src/clj/acme/email.clj
@@ -1,16 +1,11 @@
 (ns acme.email
   (:require [acme.config :as config]
             [acme.markdown :as markdown]
-            [c3kit.apron.log :as log])
-  (:import (com.amazonaws.regions Regions)
-           (com.amazonaws.services.simpleemail AmazonSimpleEmailServiceClient AmazonSimpleEmailServiceClientBuilder)
-           (com.amazonaws.services.simpleemail.model Body Content Destination Message SendEmailRequest)))
+            [c3kit.apron.log :as log]))
 
-(def ses-client
-  (delay
-    (.build
-      (doto (AmazonSimpleEmailServiceClientBuilder/standard)
-        (.withRegion Regions/US_WEST_2)))))
+;; OSS template ships only :to-log. To send real email (e.g. via AWS SES),
+;; add com.amazonaws/aws-java-sdk-ses to deps.edn and define a (defmethod
+;; client-send-email :ses [_ email] …).
 
 (defmulti client-send-email (fn [conf _] (:client conf)))
 
@@ -19,30 +14,6 @@
         (string? field) [field]
         (sequential? field) (filter string? field)
         :else (throw (ex-info "Unknown email address type" {:address field}))))
-
-(defn ses-destination [to cc bcc]
-  (doto (Destination. (->email-list to))
-    (.setCcAddresses (->email-list cc))
-    (.setBccAddresses (->email-list bcc))))
-
-(defn ses-message [subject text html]
-  (let [text (or text "Please see the HTML content of this email.")
-        html (or html (markdown/->html text))
-        body (.withHtml (Body. (Content. text)) (Content. html))]
-    (Message. (Content. subject) body)))
-
-(defmethod client-send-email :ses [_ {:keys [from to cc bcc subject text html]}]
-  (log/info "SES: sending email to:" to "-" subject)
-  (try
-    (let [dest                                   (ses-destination to cc bcc)
-          message                                (ses-message subject text html)
-          ^AmazonSimpleEmailServiceClient client @ses-client
-          request                                (SendEmailRequest. from dest message)]
-      (.sendEmail client request)
-      (log/debug "SES email sent"))
-    (catch Exception e
-      (log/error "Email error:")
-      (log/error e))))
 
 (defmethod client-send-email :to-log [_ {:keys [to cc bcc from subject text html]}]
   (let [divider (apply str (repeat 80 "="))]
